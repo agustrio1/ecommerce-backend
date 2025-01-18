5938e74f70a85bc30c912c473638d419933162ba
agustrio1 <agustrio1998@gmail.com>
2025-01-18 17:25:20 +0700

    fix: :bug: api

--- src/app.ts
+++ src/app.ts
@@ -41,7 +41,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
 app.use(cookieParser());
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.set('trust proxy', true); 
